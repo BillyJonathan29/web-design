a048e0533c02a89f21e940485a1c6d694ff9e892
BillyJonathan29 <billyjonathan048@gmail.com>
2024-12-14 21:27:39 +0700

    hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
 // hamburger
 
+// const hamburger = document.querySelector("#hamburger");
+// const navMenu = document.querySelector("#nav-menu");
+
+// hamburger.addEventListener("click", function () {
+//   hamburger.classList.toggle("hamburger-active");
+//   navMenu.classList.toggle("hidden");
+// });
+
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const lines = document.querySelectorAll(".hamburger-icons");
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
+hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("hidden");
+  lines.forEach((line) => line.classList.toggle("active"));
 });
 
 // navbar fixed
